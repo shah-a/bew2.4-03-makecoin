7782a336b53346e86dca682762d92336cd36de9f
Ali Shah <s.ashah@outlook.com>
2021-07-20 07:18:01 -0400

    [Update] seeded contract deployer's account

--- contracts/SmashFighters.sol
+++ contracts/SmashFighters.sol
@@ -8,14 +8,18 @@ contract SmashFighters is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("SSB Fighter", "SSB") {}
-
-    function awardItem(address player) public returns (uint256) {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-
-        return newItemId;
+    constructor() ERC721("SSB Fighter", "SSB") {
+        for (uint256 i = 1; i <= 12; i++) {
+            _mint(msg.sender, i);
+        }
     }
+
+    // function awardItem(address player) public returns (uint256) {
+    //     _tokenIds.increment();
+
+    //     uint256 newItemId = _tokenIds.current();
+    //     _mint(player, newItemId);
+
+    //     return newItemId;
+    // }
 }
